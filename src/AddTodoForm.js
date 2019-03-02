33a661baf123dc033798e09eec8cc577cec1017f
honai <29170100+honai@users.noreply.github.com>
2019-03-03 00:55:36 +0900

    追加フォームなどのスタイリング

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -15,11 +15,16 @@ const AddTodoForm = ({addTodo}) => {
   }
 
   return (
-    <form className='todo-form' onSubmit={handleAddTodo}>
-      <input required className='todo-input' placeholder='タスクの追加' autoComplete='off'
-        value={newTodoText} onChange={(e) => setNewTodoText(e.target.value)} />
-      <input type='date' value={newTodoDate} onChange={(e) => setNewTodoDate(e.target.value)} />
-      <button disabled={!newTodoText.length} className='add-button' type='submit'>追加</button>
+    <form className='addtodo-form' onSubmit={handleAddTodo}>
+      <div className='text-button-group'>
+        <input required className='addtodo-text' placeholder='タスクの追加' autoComplete='off'
+          value={newTodoText} onChange={(e) => setNewTodoText(e.target.value)} />
+        <button disabled={!newTodoText.length} className='add-button' type='submit'>追加</button>
+      </div>
+      <div className='deadline-group'>
+        <span className='deadline-label'>期限:</span>
+        <input type='date' className='addtodo-date' value={newTodoDate} onChange={(e) => setNewTodoDate(e.target.value)} />
+      </div>
     </form>
   );
 }
