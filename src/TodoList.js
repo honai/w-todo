e7b65793727e5f61effac2bfc5ab7857ad1320c2
honai <29170100+honai@users.noreply.github.com>
2019-03-02 22:40:20 +0900

    Todoアイテムのスタイリング

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import ClassNames from 'classnames';
+import './TodoList.css';
 
 const TodoItem = ({todo, toggleTodo}) => (
-  <li>
-    <label>
-      <input type='checkbox' checked={todo.completed} onChange={toggleTodo} />{todo.text}
+  <li className='todo-item'>
+    <label className='check-box'>
+      <i className='material-icons'>{todo.completed ? 'check_circle' : 'radio_button_unchecked'}</i>
+      <input type='checkbox' checked={todo.completed} onChange={toggleTodo} />
     </label>
+    <span className={ClassNames('todo-text', {completed: todo.completed})}>{todo.text}</span>
   </li>
 );
 
 const TodoList = ({todos, toggleTodo}) => {
   return (
-    <ul>
+    <ul className='todo-list'>
       {todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} toggleTodo={() => toggleTodo(todo.id)} />
       ))}
